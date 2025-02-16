0bd92ce0c3505f0345ab0e9e433fa97951923149
Muziljohn <muzilhassan9@gmail.com>
2025-02-16 23:50:50 +0500

    working on users

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required '],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required '],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide correct email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required '],
+    minLength: [8, 'password must be atleast 8 characters length'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'confirm password is required '],
+    validate: {
+      validator: function (val) {
+        return this.password === val;
+      },
+      message: 'confirm password must match password',
+    },
+  },
+});
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const userController = require('../controller/userController');
+const authController = require('../controller/authController');
 const Router = express.Router();
 
+Router.post('/signup', authController.signUp);
 Router.route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
